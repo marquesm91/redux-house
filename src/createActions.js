caecb0b435b661ff7a45efe55158f5debedca462
Matheus Marques <marquesm91@gmail.com>
2019-01-10 18:21:38 -0200

    add createActions

---
+++ src/createActions.js
@@ -0,0 +1,29 @@
+import * as R from 'ramda';
+import createAction from './createAction';
+import camelize from './utils/camelize';
+
+export default object => {
+  const keys = Object.keys(object);
+
+  const actions = {};
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const actionName = camelize(key);
+    const value = object[key];
+
+    if (R.is(Array, value)) {
+      const target = value[0];
+
+      if (R.length(value) === 2) {
+        const effect = value[1];
+        actions[actionName] = createAction(key, target, effect);
+      } else {
+        actions[actionName] = createAction(key, target);
+      }
+    } else {
+      actions[actionName] = createAction(key, value);
+    }
+  }
+
+  return actions;
+};
